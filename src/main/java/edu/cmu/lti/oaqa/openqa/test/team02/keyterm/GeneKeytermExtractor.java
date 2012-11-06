7425275f09e061b22bfc14daa299f991d36134bc
Kartik Mandaville <kar2905@gmail.com>
2012-11-06 17:07:46 -0500

    implementing hw1's code

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/GeneKeytermExtractor.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/GeneKeytermExtractor.java
@@ -1,7 +1,14 @@
 package edu.cmu.lti.oaqa.openqa.test.team02.keyterm;
 
+import hw1.kmandavi.CSVToHashMap;
+import hw1.kmandavi.GeneTag;
+import hw1.kmandavi.PosTagNamedEntityRecognizer;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.resource.ResourceInitializationException;
 
 import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
@@ -11,18 +18,32 @@ public class GeneKeytermExtractor extends AbstractKeytermExtractor {
   @Override
   protected List<Keyterm> getKeyterms(String question) {
     // TODO Auto-generated method stub
-    question = question.replace('?', ' ');
-    question = question.replace('(', ' ');
-    question = question.replace('[', ' ');
-    question = question.replace(')', ' ');
-    question = question.replace(']', ' ');
-    question = question.replace('/', ' ');
-    question = question.replace('\'', ' ');
+    CSVToHashMap genes = new CSVToHashMap();
 
-    String[] questionTokens = question.split("\\s+");
+    PosTagNamedEntityRecognizer Tagger = null;
+    try {
+      Tagger = new PosTagNamedEntityRecognizer();
+    } catch (ResourceInitializationException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    Map<Integer, Integer> occurences = Tagger.getGeneSpans(question);
     List<Keyterm> keyterms = new ArrayList<Keyterm>();
-    for (int i = 0; i < questionTokens.length; i++) {
-      keyterms.add(new Keyterm(questionTokens[i]));
+
+    int begin;
+    int end;
+    String gene;
+
+    for (Map.Entry<Integer, Integer> entry : occurences.entrySet())
+    {
+      begin = entry.getKey();
+      end = entry.getValue();
+      gene = question.substring(begin, end);
+      
+      if(genes.findGene(gene) == true ){
+        keyterms.add(new Keyterm(gene));
+
+      }
     }
 
     return keyterms;
