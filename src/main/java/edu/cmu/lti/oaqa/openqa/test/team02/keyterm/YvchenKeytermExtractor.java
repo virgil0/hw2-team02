5807b3077ff8e602fff7c1f1e888a9cab05b47df
yvchen <yvchen@cs.cmu.edu>
2012-11-14 20:52:43 -0500

    stemming+stopword

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/YvchenKeytermExtractor.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/YvchenKeytermExtractor.java
@@ -1,9 +1,16 @@
 package edu.cmu.lti.oaqa.openqa.test.team02.keyterm;
 
+import hw1.kmandavi.CSVToHashMap;
+import hw1.kmandavi.GeneTag;
+import hw1.kmandavi.PosTagNamedEntityRecognizer;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.resource.ResourceInitializationException;
@@ -12,6 +19,13 @@ import com.aliasi.chunk.Chunk;
 import com.aliasi.chunk.Chunker;
 import com.aliasi.chunk.Chunking;
 import com.aliasi.util.AbstractExternalizable;
+import com.aliasi.tokenizer.EnglishStopTokenizerFactory;
+import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
+import com.aliasi.tokenizer.LowerCaseTokenizerFactory;
+import com.aliasi.tokenizer.PorterStemmerTokenizerFactory;
+import com.aliasi.tokenizer.RegExFilteredTokenizerFactory;
+import com.aliasi.tokenizer.Tokenizer;
+import com.aliasi.tokenizer.TokenizerFactory;
 
 import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
 import edu.cmu.lti.oaqa.framework.data.Keyterm;
@@ -42,13 +56,26 @@ public class YvchenKeytermExtractor extends AbstractKeytermExtractor {
   @Override
   protected List<Keyterm> getKeyterms(String question) {
     List<Keyterm> keytermList = new ArrayList<Keyterm>();
-
+    
+    // stemming + stop word removal
+    TokenizerFactory f1 = IndoEuropeanTokenizerFactory.INSTANCE;
+    TokenizerFactory f2 = new LowerCaseTokenizerFactory(f1);
+    TokenizerFactory f3 = new PorterStemmerTokenizerFactory(f2);
+    TokenizerFactory f4 = new EnglishStopTokenizerFactory(f3);
+    TokenizerFactory f5 = new RegExFilteredTokenizerFactory(f4, Pattern.compile("[a-z0-9]+"));
+    Tokenizer nowTokenizer = f5.tokenizer(question.toCharArray(), 0, question.length());
+    String[] words = nowTokenizer.tokenize();
+    for (String word : words) {
+      keytermList.add(new Keyterm(word));
+      System.out.println("********************  " + word);
+    }
+    /*
     Chunking chunking = chunker.chunk(question);
     for (Chunk chunk : chunking.chunkSet()) {
       String keyterm = question.substring(chunk.start(), chunk.end());
       keytermList.add(new Keyterm(keyterm));
     }
-
+    */
     return keytermList;
   }
 }
