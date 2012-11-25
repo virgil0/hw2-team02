7835779928bb96c02f6747dfb4ae70da81f0b37e
yvchen <yvchen@cs.cmu.edu>
2012-11-24 20:46:06 -0500

    Add LingPipe new model

--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/FxieKeytermExtractor.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/FxieKeytermExtractor.java
@@ -1,17 +1,30 @@
 package edu.cmu.lti.oaqa.openqa.test.team02.keyterm;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+import org.htmlparser.visitors.HtmlPage;
 
 import com.aliasi.chunk.Chunk;
 import com.aliasi.chunk.Chunker;
 import com.aliasi.chunk.Chunking;
+import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
+import com.aliasi.tokenizer.RegExFilteredTokenizerFactory;
+import com.aliasi.tokenizer.TokenizerFactory;
 import com.aliasi.util.AbstractExternalizable;
 
 import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
@@ -21,12 +34,46 @@ import edu.cmu.lti.oaqa.framework.data.Keyterm;
  *
  * @author Fei Xie
  */
+
 public class FxieKeytermExtractor extends AbstractKeytermExtractor {
 
   public static final String PARAM_MODELFILE = "model_file";
 
   private Chunker chunker;
 
+  public static String getWebCon(String domain) throws IOException {
+
+    System.out.println("START READING FROM...("+domain+")");
+
+    StringBuilder sb = new StringBuilder();
+
+    try {
+
+      java.net.URL url = new java.net.URL(domain);
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+      String line;
+
+      while (in.ready()) {
+
+        line = in.readLine();
+        sb.append(line);
+
+      }
+
+      in.close();
+
+    } catch (FileNotFoundException e) { 
+
+      sb.append(e.toString());
+
+      System.err.println("cannot find Synonyms so continue");       
+
+    }
+    return sb.toString();
+
+  }
   @Override
   public void initialize(UimaContext c) throws ResourceInitializationException {
     super.initialize(c);
@@ -47,11 +94,57 @@ public class FxieKeytermExtractor extends AbstractKeytermExtractor {
   @Override
   protected List<Keyterm> getKeyterms(String question) {
     List<Keyterm> keytermList = new ArrayList<Keyterm>();
-
+    final String s = "http://words.bighugelabs.com/";
+    String c = null;
     Chunking chunking = chunker.chunk(question);
     for (Chunk chunk : chunking.chunkSet()) {
       String keyterm = question.substring(chunk.start(), chunk.end());
+      String nowkeyterm = null;
       keytermList.add(new Keyterm(keyterm));
+
+      Matcher m = Pattern.compile("[A-Z0-9]+").matcher(keyterm);
+      if (m.matches() == false){
+        System.out.println("*****Find Synonym**********" + keyterm);
+        try {
+          c = getWebCon(s + keyterm).toString();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        Parser parser = Parser.createParser(c, "GBK");         
+        HtmlPage page = new HtmlPage(parser);                              
+        TagNameFilter filter=  new TagNameFilter("li");
+        org.htmlparser.Node node = null;  
+        NodeList nodeList = null;
+        try {
+          nodeList = parser.extractAllNodesThatMatch(filter);
+        } catch (ParserException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        int limit = nodeList.size();
+        if(nodeList.size() > 5)
+          limit = 5;
+        for (int i = 0; i < limit; ++i) 
+        {  
+            node = nodeList.elementAt(i);
+            if(!node.getParent().getPreviousSibling().getChildren().asString().equals("noun"))
+            {                     
+                continue;
+            }
+            else
+              {
+                String synonym = node.getChildren().asString();
+                Chunking nowchunking = chunker.chunk(synonym);
+                System.out.println("!!!Synonym!!!!!!!!!!! " + synonym);
+                keytermList.add(new Keyterm(synonym));
+/*                for (Chunk nowchunk : nowchunking.chunkSet()) {
+                  nowkeyterm = synonym.substring(nowchunk.start(), nowchunk.end());
+                  keytermList.add(new Keyterm(nowkeyterm));
+                  System.out.println("!!!New Synonym!!!!!!! " + nowkeyterm);
+                }*/              
+              }
+         }
+      }
     }
 
     return keytermList;
--- src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/YvchenKeytermExtractor.java
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team02/keyterm/YvchenKeytermExtractor.java
@@ -5,8 +5,12 @@ import hw1.kmandavi.GeneTag;
 import hw1.kmandavi.PosTagNamedEntityRecognizer;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -18,12 +22,16 @@ import org.apache.uima.resource.ResourceInitializationException;
 import com.aliasi.chunk.Chunk;
 import com.aliasi.chunk.Chunker;
 import com.aliasi.chunk.Chunking;
+import com.aliasi.hmm.HiddenMarkovModel;
+import com.aliasi.hmm.HmmDecoder;
 import com.aliasi.util.AbstractExternalizable;
+import com.aliasi.tag.Tagging;
 import com.aliasi.tokenizer.EnglishStopTokenizerFactory;
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
 import com.aliasi.tokenizer.LowerCaseTokenizerFactory;
 import com.aliasi.tokenizer.PorterStemmerTokenizerFactory;
 import com.aliasi.tokenizer.RegExFilteredTokenizerFactory;
+import com.aliasi.tokenizer.RegExTokenizerFactory;
 import com.aliasi.tokenizer.Tokenizer;
 import com.aliasi.tokenizer.TokenizerFactory;
 
@@ -55,19 +63,63 @@ public class YvchenKeytermExtractor extends AbstractKeytermExtractor {
 
   @Override
   protected List<Keyterm> getKeyterms(String question) {
-    List<Keyterm> keytermList = new ArrayList<Keyterm>();
+    FileInputStream fileIn;
+    HmmDecoder decoder = null;
+    try {
+      fileIn = new FileInputStream("src/main/resources/mode/pos-en-biogenia.HiddenMarkovModel");
+      ObjectInputStream objIn = null;
+      try {
+        objIn = new ObjectInputStream(fileIn);
+        HiddenMarkovModel hmm = (HiddenMarkovModel) objIn.readObject();
+        objIn.close();
+        decoder = new HmmDecoder(hmm);
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    Tokenizer nowTokenizer;
+    String[] tokens;
+    List<String> tokenList;
+    List<Keyterm> keytermList = null;
     
     // stemming + stop word removal
-    TokenizerFactory f1 = IndoEuropeanTokenizerFactory.INSTANCE;
-    TokenizerFactory f2 = new LowerCaseTokenizerFactory(f1);
+    TokenizerFactory f0 = IndoEuropeanTokenizerFactory.INSTANCE;
+    TokenizerFactory f1 = new RegExTokenizerFactory("(-|'|\\d|\\p{L})+|\\S");
+    nowTokenizer = f1.tokenizer(question.toCharArray(), 0, question.length());
+    tokens = nowTokenizer.tokenize();
+    tokenList = Arrays.asList(tokens);
+    Tagging<String> tagging = decoder.tag(tokenList);
+    for (int i = 0; i < tagging.size(); ++ i)
+      System.out.print(tagging.token(i) + "_" + tagging.tag(i) + " ");
+    
+    TokenizerFactory f2 = new LowerCaseTokenizerFactory(f0);
+    TokenizerFactory f6 = new RegExFilteredTokenizerFactory(f2, Pattern.compile("([a-z0-9 ]+)"));
+    nowTokenizer = f6.tokenizer(question.toCharArray(), 0, question.length());
+    tokens = nowTokenizer.tokenize();
+    for (String token : tokens) {
+      keytermList.add(new Keyterm(token));
+      System.out.println("===================  " + token);
+    }
+    
     TokenizerFactory f3 = new PorterStemmerTokenizerFactory(f2);
     TokenizerFactory f4 = new EnglishStopTokenizerFactory(f3);
     TokenizerFactory f5 = new RegExFilteredTokenizerFactory(f4, Pattern.compile("[a-z0-9]+"));
-    Tokenizer nowTokenizer = f5.tokenizer(question.toCharArray(), 0, question.length());
-    String[] words = nowTokenizer.tokenize();
-    for (String word : words) {
-      keytermList.add(new Keyterm(word));
-      System.out.println("********************  " + word);
+    nowTokenizer = f5.tokenizer(question.toCharArray(), 0, question.length());
+    tokens = nowTokenizer.tokenize();
+    
+    for (String token : tokens) {
+      keytermList.add(new Keyterm(token));
+      System.out.println("********************  " + token);
     }
     /*
     Chunking chunking = chunker.chunk(question);
